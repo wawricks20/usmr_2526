0062201fd5de52a8c98a867426327f36a6da0b3d
wawricks20 <kirillkojaevuniversity@gmail.com>
2025-11-10 02:42:32 +0000

    changed name

---
+++ SamplingSeahorses.R
@@ -0,0 +1,32 @@
+# Start by getting your data!
+# IMPORTANT: change the "asymptotic_arias" to be your group name!  
+source("https://edin.ac/42QTz8T")
+get_my_data(group_name = "sampling_seahorses")
+
+# after running the above line of code will, the various datasets should be in your environment
+pilotA
+pilotB
+pilotC
+nudges
+followup
+
+# below here, you can add R code to address the questions in the assignment
+
+### -----------------------------------------------------------------------
+
+### 1. Pilot studies ---
+
+
+
+
+
+### 2. Nudge nudge, wink wink ---
+
+
+
+
+
+### 3. The uninstalled ---
+
+
+
