016414c77e80f6a0bce8b46742593f37a323ceb9
wawricks20 <kirillkojaevuniversity@gmail.com>
2025-11-20 20:00:19 +0000

    Update SamplingSeahorses.R

--- SamplingSeahorses.R
+++ SamplingSeahorses.R
@@ -18,26 +18,28 @@ followup
 ### -----------------------------------------------------------------------
 
 ### 1. Pilot studies ---
-  ### Question A - Pilot study A
-    ### Extracting rows that contain "40 George Square" OR "40GS" using a pattern
+# Question A - Pilot study A
 pilotA <- pilotA |>
-  filter(grepl("40 George Square", room) | grepl("40GS", room))
+  filter(
+    !startsWith(room, "NA ")
+  )
+# identifying and removing duplicates (keeping first ones)
+pilotA |> 
+  count(room, sort = TRUE) |>
+  filter(n>1)
+pilotA <- pilotA |>
+  group_by(room) |>
+  slice(1) |>
+  ungroup()
 dim(pilotA)
-    ### Removing duplicates(1.01, 13.07, 3.01, 7.18, LG07, Lecture 7)
-pilotA <- pilotA[-c(4, 21, 19, 17, 22, 23, 6), ]
-    ### Renaming "NA 40GS_LG.07" to "40 George Square 40GS_LG.07"
-pilotA[3,1] <- "40 George Square 40GS_LG.07"
-
-
-
-    ### Assigning labels for values
+# Assigning labels for values
 pilotA$lights <- factor(pilotA$lights,
                         levels = c(0,1),
                         labels = c("Off", "On"))
 pilotA$condition <- factor(pilotA$condition,
                            levels = c(0,1,2),
                            labels = c("Nothing", "A Written Message", "Googly Eyes"))
-    ### Data Visualisation of lights left on vs off
+# Data Visualisation
 pilotA_table <- table(pilotA$condition, pilotA$lights)
 pilotA_table
 pilotA_graph <- ggplot(pilotA) +
@@ -46,31 +48,30 @@ pilotA_graph <- ggplot(pilotA) +
   labs(
     title = "Lights Off vs On"
   ) +
-  scale_y_continuous(breaks = c(0,2,4,6)) +
   theme_classic() +
-  scale_fill_manual(values = c("grey", "coral"))
-  
+  scale_fill_manual(values = c("salmon2", "darkseagreen2"))
 pilotA_graph
-    ### Assumption - Violated (cells had expected counts below 5)
+# Excepted cell assumption
 chisq.test(pilotA_table)$expected
-    ### Analysis - Not significant, p >- .05
+# Analysis - Significant, p = .038
 chisq.test(pilotA_table)
-  
-  ### Question B - Pilot study B
-    ### Data Cleaning (Standardising names)
+plot(pilotA_table, main = "Contingency Table of Light Status by Condition ")
+
+# Question B - Pilot study B
+# Data Cleaning
 pilotB <- pilotB |>
   mutate(
     discount = replace(discount, 1:20, "50p keep cup discount"),
     discount = replace(discount, 21:40, "no discount")
   )
-    ### Descriptive statistics
+# Descriptive statistics
 pilotB_descriptives <- pilotB|>
   group_by(discount) |>
   summarise(
     coffees_avg = mean(ncoffees),
     coffees_sd = sd(ncoffees)
   )
-    ### Descriptive plot
+# Descriptive plot
 pilotB_graph <- ggplot(pilotB) +
   aes(x = discount, y = ncoffees, fill = discount) +
   geom_boxplot(
@@ -82,38 +83,38 @@ pilotB_graph <- ggplot(pilotB) +
     x = "Discount"
   ) +
   theme_classic() +
-  scale_fill_manual(values = c("skyblue", "coral")) +
+  scale_fill_manual(values = c("darkseagreen2", "salmon2")) +
   theme(legend.position = "none")
-
 pilotB_graph
-    ### Normality assumption checks (Normal distribution, p > .05)
+# Normality assumption checks (Normal distribution, p > .05)
 shapiro.test(pilotB$ncoffees[pilotB$discount == "50p keep cup discount"])
 shapiro.test(pilotB$ncoffees[pilotB$discount == "no discount"])
-    ### Analysis (Not Significant, p> .05)
+# Analysis (Not Significant, p> .05)
 t.test(ncoffees ~ discount, data = pilotB, alternative = "greater")
-  ### Question C - Pilot study C
-    ### Removing N/A entries, changing device usage to hours
-pilotC <- pilotC|>
-  filter(!is.na(device_usage)) |>
-  mutate(device_usage = device_usage/60)
-  
-    ### Descriptive statistics
-dim(pilotC) 
 
+# Question C - Pilot study C
+# Remove N/A entries 
+pilotC <- pilotC |> filter(!is.na(device_usage)) 
+# Test for normality - Initial
+qqnorm(pilotC$device_usage, main="Q-Q Plot: Device Usage")
+qqline(pilotC$device_usage, col="salmon2") 
+shapiro.test(pilotC$device_usage)
+pilotCmod <- lm(env_concern ~ device_usage, data = pilotC)
+plot(pilotCmod)
+# Remove outliers 
+pilotC <- pilotC[-c(9,29), ] 
+# Test for normality - After removing outliers
+pilotCmod1 <- lm(env_concern ~ device_usage, data = pilotC)
+plot(pilotCmod1)
+# Descriptive Statistics
+dim(pilotC) 
 pilotCdescriptives <- pilotC |>
   select(device_usage, env_concern) |>
   describe() 
-
 pilotCdescriptives
-    ### Normality assumption checks
-qqnorm(pilotC$device_usage, main="Q-Q Plot: Device Usage")
-qqline(pilotC$device_usage, col="red") 
-
-qqnorm(pilotC$env_concern, main="Q-Q Plot: Environmental Concern")
-qqline(pilotC$env_concern, col="red")
-    ### Analysis
-cor.test(pilotC$device_usage, pilotC$env_concern)
-    ### Graphical representation
+# Testing association 
+cor.test(pilotC$device_usage, pilotC$env_concern) 
+# Graphical representation 
 ggplot(pilotC, aes(x= device_usage, y= env_concern)) + 
   geom_point() +
   labs(x= "Device Usage (hours per day)",
@@ -121,7 +122,7 @@ ggplot(pilotC, aes(x= device_usage, y= env_concern)) +
        title = "Pilot Study C",
        subtitle = "Relationship Between Device Usage and Environmental Concern") +
   theme_classic() +
-  geom_smooth(method=lm, se= FALSE, color= "brown") 
+  geom_smooth(method=lm, se= FALSE, color= "salmon2") 
 ### 2. Nudge nudge, wink wink ---
 
 
